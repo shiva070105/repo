7dacd9f432a7ba83051204d7bf903ad0b28b6b3a
shiva <155425684+shiva070105@users.noreply.github.com>
2024-06-09 13:33:43 +0530

    Update Ml5.py

--- Ml5.py
+++ Ml5.py
@@ -1,44 +1,52 @@
-# import necessary libarities
+import streamlit as st
 import pandas as pd
-from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-# load data from CSV
+# Load data from CSV
 data = pd.read_csv('tennisdata.csv')
-print("THe first 5 values of data is :\n",data.head())
+st.write("The first 5 values of the data are:")
+st.write(data.head())
 
-# obtain Train data and Train output
+# Obtain Train data and Train output
 X = data.iloc[:,:-1]
-print("\nThe First 5 values of train data is\n",X.head())
+st.write("The first 5 values of train data are:")
+st.write(X.head())
 
 y = data.iloc[:,-1]
-print("\nThe first 5 values of Train output is\n",y.head())
+st.write("The first 5 values of Train output are:")
+st.write(y.head())
 
-# Convert then in numbers 
+# Convert them to numbers
 le_outlook = LabelEncoder()
-X.Outlook = le_outlook.fit_transform(X.Outlook)
+X['Outlook'] = le_outlook.fit_transform(X['Outlook'])
 
 le_Temperature = LabelEncoder()
-X.Temperature = le_Temperature.fit_transform(X.Temperature)
+X['Temperature'] = le_Temperature.fit_transform(X['Temperature'])
 
 le_Humidity = LabelEncoder()
-X.Humidity = le_Humidity.fit_transform(X.Humidity)
+X['Humidity'] = le_Humidity.fit_transform(X['Humidity'])
 
 le_Windy = LabelEncoder()
-X.Windy = le_Windy.fit_transform(X.Windy)
+X['Windy'] = le_Windy.fit_transform(X['Windy'])
 
-print("\nNow the Train data is :\n",X.head())
+st.write("Now the Train data is:")
+st.write(X.head())
 
 le_PlayTennis = LabelEncoder()
 y = le_PlayTennis.fit_transform(y)
-print("\nNow the Train output is\n",y)
+st.write("Now the Train output is:")
+st.write(y)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20)
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
+# Train the model
 classifier = GaussianNB()
-classifier.fit(X_train,y_train)
+classifier.fit(X_train, y_train)
 
-from sklearn.metrics import accuracy_score
-print("Accuracy is:",accuracy_score(classifier.predict(X_test),y_test))
+# Calculate accuracy
+accuracy = accuracy_score(classifier.predict(X_test), y_test)
+st.write("Accuracy is:", accuracy)
